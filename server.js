f540ce34efb7621d508845f6ecac9a13dfbd57b7
John Kim <yohany@gmail.com>
2014-03-02 00:41:38 +0900

    init server.js

--- server.js
+++ server.js
@@ -17,27 +17,34 @@ try {
   process.exit(1);
 }
 
-config.host = 'session.xpush.io';
+config.host = 'stalk.xpush.io';
 config.port = 8000;
 
 var server = xpush.createSessionServer(config);
 
-
-server.static(/\/public\/?.*/, {
-  directory: __dirname+'/public'
-});
 
 server.once('connected', function (url, port){
   console.log('listen - '+url+':'+port);
 });
 
 server.on('oauth', function (data){
+  // data.request.user.displayName
+  
   data.response.send('Welcome ' + data.request.user.displayName);
 });
 
 
+/* default xpush oauth providers */
+// FACEBOOK Auth Url : /auth/facebook
+// FACEBOOK Callback Url : /auth/facebook/callback
 
-server.get('/test', function (req, res, next) {
-  res.send({status : 'pong'});
-  next();
-});
+
+//server.static(/\/public\/?.*/, {
+//  directory: __dirname+'/public'
+//});
+
+//server.get('/test', function (req, res, next) {
+//  res.send({status : 'pong'});
+//  next();
+//});
+
